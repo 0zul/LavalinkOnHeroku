842160f9b408884b862cea093a8de834758c26ae
Looney <devnilghosh2002@gmail.com>
2020-05-10 21:51:30 +0530

    v1.0.0 auto

---
+++ bootstrap.js
@@ -0,0 +1,55 @@
+
+const fs = require('fs')
+const http = require('http')
+let application = fs.readFileSync('./application.yml', 'utf8')
+
+if (process.env.PORT) {
+    application = application.replace('DYNAMICPORT', process.env.PORT)
+}
+
+if (process.env.PASS) {
+    application = application.replace('youshallnotpass', process.env.PASS)
+}
+fs.writeFileSync('./application.yml', application)
+
+const download = function (url, dest, cb) { //modified code from https://stackoverflow.com/a/22907134
+    const file = fs.createWriteStream(dest);
+    http.get(url, function (response) {
+        response.pipe(file);
+        console.log('Downloading Lavalink.jar...')
+        file.on('finish', function () {
+            console.log('Downloaded Lavalink.jar')
+            file.close(cb);
+        });
+    }).on('error', function (err) {
+        fs.unlinkSync(dest);
+        console.error(err)
+    });
+};
+
+function startLavalink() {
+    const spawn = require('child_process').spawn;
+    const child = spawn('java', ['-jar', 'Lavalink.jar'])
+
+    child.stdout.setEncoding('utf8')
+    child.stderr.setEncoding('utf8')
+
+    child.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(data);
+    });
+
+    child.on('error', (error) => {
+        console.error(error);
+    });
+
+    child.on('close', (code) => {
+        console.log(`Lavalink exited with code ${code}`);
+    });
+}
+
+const cdn = 'https://cdn.glitch.com/5d4f310e-9fae-4c7b-968d-7bf316844140%2FLavalink.jar?v=1589125085827'
+download(cdn, './Lavalink.jar', startLavalink)
